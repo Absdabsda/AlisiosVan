336644f624c3ef1f27f9411d230b7c3dd9f1b7c4
Julia <juliavalen9@gmail.com>
2025-10-01 22:49:00 +0200

    manage reserva, solo acceso a traves del link mandado por correo ++seguridad

--- config/i18n/lang/en/banner.php
+++ config/i18n/lang/en/banner.php
@@ -3,4 +3,25 @@ return [
 "Do you want to continue your search from %s to %s?" => "Do you want to continue your search from %s to %s?",
 "Resume search" => "Resume search",
 "Dismiss" => "Dismiss",
+
+    //find reservation
+    'find.title'         => 'Find your reservation',
+    'find.subtitle'      => 'Enter your reservation number and email to receive a secure link.',
+    'find.form.resid'    => 'Reservation #',
+    'find.form.email'    => 'Email',
+    'find.form.send'     => 'Send link',
+    'find.alert.sent'    => 'If a reservation matched, we have sent a secure link to your email address.',
+    'find.btn.home'      => 'Back to Home',
+    'find.btn.retry'     => 'Try again',
+
+    'mail.subject'       => 'Your reservation access link',
+    'mail.intro'         => 'Hello! Here is your secure link to manage your reservation.',
+    'mail.open'          => 'Open management page',
+    'mail.ignore'        => 'If you did not request this, you can ignore this email.',
+    'mail.footer'        => 'Alisios Van',
+    'find.mail.subject' => 'Your reservation access link',
+    'find.mail.intro'   => 'Hello! Here is your secure link to manage your reservation.',
+    'find.mail.button'  => 'Open management page',
+    'find.mail.ignore'  => 'If you did not request this, you can ignore this email.',
+
 ];
--- config/i18n/lang/es/banner.php
+++ config/i18n/lang/es/banner.php
@@ -3,4 +3,25 @@ return [
 "Do you want to continue your search from %s to %s?" => "¿Quieres continuar tu búsqueda del %s al %s?",
 "Resume search" => "Reanudar búsqueda",
 "Dismiss" => "Ocultar",
+
+    //find reservation
+    'find.title'         => 'Encuentra tu reserva',
+    'find.subtitle'      => 'Introduce tu número de reserva y tu correo para recibir un enlace seguro.',
+    'find.form.resid'    => 'Número de reserva',
+    'find.form.email'    => 'Correo electrónico',
+    'find.form.send'     => 'Enviar enlace',
+    'find.alert.sent'    => 'Si la reserva coincide, hemos enviado un enlace seguro a tu correo electrónico.',
+    'find.btn.home'      => 'Volver al inicio',
+    'find.btn.retry'     => 'Intentar de nuevo',
+
+    'mail.subject'       => 'Tu enlace de acceso a la reserva',
+    'mail.intro'         => '¡Hola! Aquí tienes tu enlace seguro para gestionar tu reserva.',
+    'mail.open'          => 'Abrir página de gestión',
+    'mail.ignore'        => 'Si no has solicitado esto, puedes ignorar este correo.',
+    'mail.footer'        => 'Alisios Van',
+    'find.mail.subject' => 'Enlace de acceso a tu reserva',
+    'find.mail.intro'   => '¡Hola! Aquí tienes tu enlace seguro para gestionar tu reserva.',
+    'find.mail.button'  => 'Abrir página de gestión',
+    'find.mail.ignore'  => 'Si no solicitaste esto, puedes ignorar este correo.',
+
 ];
