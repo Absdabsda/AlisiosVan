9d7c949a8729077bf2491c05036d2a0f9beff367
Julia <juliavalen9@gmail.com>
2025-10-05 18:31:10 +0200

    correccion correo cancelacion

--- config/i18n/lang/de/manage.php
+++ config/i18n/lang/de/manage.php
@@ -3,7 +3,7 @@ return [
 'email.cancel.subject'       => 'Reservierung Nr. %d storniert',
 'email.cancel.header'        => 'Reservierung storniert',
 'email.cancel.lead.admin'    => 'Deine Reservierung wurde von unserem Team storniert. Wir haben eine vollständige Rückerstattung der Anzahlung auf die ursprüngliche Zahlungsmethode veranlasst (falls zutreffend). Sie erscheint in der Regel innerhalb von 5–10 Werktagen – abhängig von deiner Bank.',
-'email.cancel.lead.customer' => 'Deine Reservierung wurde wie gewünscht storniert. Gemäß unserer Richtlinie ist die Anzahlung (entspricht 1 Nacht) nicht erstattungsfähig. Es werden keine weiteren Gebühren erhoben.',
+'email.cancel.lead.customer' => 'Deine Reservierung wurde wie gewünscht storniert. Gemäß unserer Richtlinie ist die Anzahlung nicht erstattungsfähig. Es werden keine weiteren Gebühren erhoben.',
 'email.cancel.summary'       => 'Zusammenfassung:',
 'email.cancel.mistake'       => 'War das ein Versehen? Antworte auf diese E-Mail und wir helfen dir.',
 
