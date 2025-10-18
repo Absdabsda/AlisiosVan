0a839bd209167304ca8b0f8af9e19fe6c0d7ff1b
Julia <juliavalen9@gmail.com>
2025-10-18 19:14:15 +0200

    intento de SEO index.php (h1,h2) +  traducciones + titulo + meta cambios

---
+++ config/i18n/lang/en/landing.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'Camper van rental in Fuerteventura' => 'Camper van rental in Fuerteventura',
+
+    'Why travel with Alisios Van?' => 'Why travel with Alisios Van?',
+    'Camper van rental in Fuerteventura with airport pickup and insurance included' => 'Camper van rental in Fuerteventura with airport pickup and insurance included',
+    'What our travelers say' => 'What our travelers say',
+    'Our gallery of camper van rental in Fuerteventura' => 'Our gallery of camper van rental in Fuerteventura',
+    'Find us in Fuerteventura' => 'Find us in Fuerteventura',
+    'Ready to rent your camper van?' => 'Ready to rent your camper van?',
+];
---
+++ config/i18n/lang/es/landing.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'Camper van rental in Fuerteventura' => 'Alquiler de furgonetas camper en Fuerteventura',
+
+    'Why travel with Alisios Van?' => '¿Por qué viajar con Alisios Van?',
+    'Camper van rental in Fuerteventura with airport pickup and insurance included' => 'Alquiler de furgonetas camper en Fuerteventura con recogida en el aeropuerto y seguro incluido',
+    'What our travelers say' => 'Lo que dicen nuestros viajeros',
+    'Our gallery of camper van rental in Fuerteventura' => 'Nuestra galería de alquiler de furgonetas camper en Fuerteventura',
+    'Find us in Fuerteventura' => 'Encuéntranos en Fuerteventura',
+    'Ready to rent your camper van?' => '¿Listo para alquilar tu furgoneta camper?',
+];
